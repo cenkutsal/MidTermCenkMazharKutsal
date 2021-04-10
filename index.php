0096260b9a88de1608f010df4a5008ee1afdcd35
cenkutsal <1501020024@stu.iku.edu.tr>
2021-04-10 10:36:08 +0300

    index.php update

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
+<h1>Welcome</h1>
+<form method="GET">
+    <p><label for="input">License Plate</label>
+        <input type="text" name="LicencePlate" size="20" />
+    </p>
+    <p><label for="input">Brand</label>
+        <input type="text" name="Brand" size="20" />
+    </p>
+    <p><label for="input">Model</label>
+        <input type="text" name="Model" size="20" />
+    </p>
+    <p><label for="otherinput">Model Year</label>
+        <input type="text" name="ModelYear" size="5" />
+    </p>
+    <p><label for="otherinput">Color</label>
+        <input type="text" name="Color" size="5" />
+    </p>
+    <input type="submit" />
+</form>
+<pre>
+$_GET:
 <?php
-echo "welcome";
+print_r($_GET);
+?>
+</pre>
