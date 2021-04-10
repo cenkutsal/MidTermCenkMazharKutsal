538ed2b001bf6e9bfe1c4053571a0acf7bd27544
cenkutsal <1501020024@stu.iku.edu.tr>
2021-04-10 10:51:16 +0300

    index.php update

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <h1>Welcome</h1>
 <form method="GET">
-    <p><label for="input">License Plate</label>
+    <p><label for="LicencePlate">License Plate</label>
         <input type="text" name="LicencePlate" size="20" />
     </p>
-    <p><label for="input">Brand</label>
+    <p><label for="Brand">Brand</label>
         <input type="text" name="Brand" size="20" />
     </p>
-    <p><label for="input">Model</label>
+    <p><label for="Model">Model</label>
         <input type="text" name="Model" size="20" />
     </p>
-    <p><label for="otherinput">Model Year</label>
+    <p><label for="ModelYear">Model Year</label>
         <input type="text" name="ModelYear" size="5" />
     </p>
-    <p><label for="otherinput">Color</label>
+    <p><label for="Color">Color</label>
         <input type="text" name="Color" size="5" />
     </p>
-    <input type="submit" />
+    <input value="Submit" type="submit" />
 </form>
 <pre>
 $_GET:
